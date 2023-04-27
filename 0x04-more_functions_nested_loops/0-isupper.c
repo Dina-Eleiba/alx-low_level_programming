29cab2f803214b5755bc6fdbfc1e696d852aee71
Dina-Eleiba <dina.adel@outlook.com>
2023-04-27 14:07:29 -0700

    uu

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _isupper_check if c is upper
+* 
+* @c : input for alphabet
+*
+* Return: 1 if it is uppercase, 0 if not
+*/
+
+int _isupper(intc)
+{
+if (c>= 65 && c <= 90)
+	return(1);
+else
+	return(0);
+]
