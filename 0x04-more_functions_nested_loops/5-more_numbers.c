feb12c7dd600577fc258ab7f5261a39e4e5a66f3
Dina-Eleiba <dina.adel@outlook.com>
2023-04-27 17:03:27 -0700

    ggg

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,16 @@ void more_numbers(void)
 int i;
 int num;
 
-for (i = 1; i <= 10;
-{ 
+for (i = 1; i <= 10; i++)
+{
 for (num = 0; num <= 14; num++)
 {
 if (num >= 10)
 _putchar(num / 10 + '0');
+
 _putchar(num % 10 + '0');
 }
 _putchar('\n');
 }
 }
+
