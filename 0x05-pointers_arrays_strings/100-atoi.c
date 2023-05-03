58357850b26212d40e30cbf8e7a267443f5dcb43
Dina-Eleiba <dina.adel@outlook.com>
2023-05-02 18:02:53 -0700

    rre

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+atoi-converts a string to an integer
+int _atoi (char *s)
+unsigned int num = 0;
+int sign = 1;
+do 
+{
+if (*s == '_')
+sign *= -1;
+else if (*s >= 0 && *s <= '9¹)
+num= (num * 10) + (*s- '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num * sign);
+}
